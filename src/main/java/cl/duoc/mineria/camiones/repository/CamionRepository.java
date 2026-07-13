9db84f2ce8313c27370514e40eb67a0c208fbcf4
zBleend <juancrichile@gmail.com>
2026-07-13 00:38:34 -0400

    Remove unused @Repository annotation from CamionRepository

--- src/main/java/cl/duoc/mineria/camiones/repository/CamionRepository.java
+++ src/main/java/cl/duoc/mineria/camiones/repository/CamionRepository.java
@@ -2,10 +2,8 @@ package cl.duoc.mineria.camiones.repository;
 
 import cl.duoc.mineria.camiones.model.Camion;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
-@Repository
 public interface CamionRepository extends JpaRepository<Camion, Long> {
     Optional<Camion> findByPatente(String patente);
 }
